becfb5fbdb41c3c8e3679512d28e298e97ad53cc
Petar Scepanovic <hiborija@gmail.com>
2019-07-12 20:04:32 +0200

    test

--- data/dbConfig.js
+++ data/dbConfig.js
@@ -1,20 +1,20 @@
 const knex = require('knex');
 const config = require('../knexfile.js');
 
-module.exports = knex(config.development);
+// module.exports = knex(config.development);
 
-// let client
+let client
 
-// switch(process.env.NODE_ENV) {
-//     case 'production':
-//         client = knex(config.production) //for heroku postgres
-//         break
-//     case 'test':
-//         client = knex(config.testing)
-//         break
-//     default:
-//         client = knex(config.development) //for testing and running SQLite
-// }
+switch(process.env.NODE_ENV) {
+    case 'production':
+        client = knex(config.production) //for heroku postgres
+        break
+    case 'test':
+        client = knex(config.testing)
+        break
+    default:
+        client = knex(config.development) //for testing and running SQLite
+}
 
 
-// module.exports = client
+module.exports = client
--- knexfile.js
+++ knexfile.js
@@ -32,23 +32,41 @@ module.exports = {
       directory: './data/seeds'
     }
   },
-
   production: {
-    client: 'pg',
-    // connection: process.env.DATABASE_URL + '?ssl=true',
+    client: 'sqlite3',
     connection: {
       filename: './data/wunderlist.sqlite3'
     },
-    pool: {
-      min: 2,
-      max: 10
-    },
+    useNullAsDefault: true,
     migrations: {
-      tableName: 'knex_migrations',
       directory: './data/migrations'
     },
     seeds: {
       directory: './data/seeds'
+    },
+    pool: {
+      afterCreate: (conn, done) => {
+        conn.run('PRAGMA foreign_keys = ON', done); //enforce FK
+      }
     }
-  }
+  },
+
+  // production: {
+  //   client: 'pg',
+  //   // connection: process.env.DATABASE_URL + '?ssl=true',
+  //   connection: {
+  //     filename: './data/wunderlist.sqlite3'
+  //   },
+  //   pool: {
+  //     min: 2,
+  //     max: 10
+  //   },
+  //   migrations: {
+  //     tableName: 'knex_migrations',
+  //     directory: './data/migrations'
+  //   },
+  //   seeds: {
+  //     directory: './data/seeds'
+  //   }
+  // }
 };
